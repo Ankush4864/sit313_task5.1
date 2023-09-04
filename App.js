b29e744fd4aa88ae732fdd5aff62e4057de40f4c
Ankush4864 <132539800+Ankush4864@users.noreply.github.com>
2023-09-04 23:14:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import PostPage from './Page1';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+   < PostPage/>
+   </div>
+  );
+}
+
+export default App;
